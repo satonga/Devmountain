92f436f80a3926bfbd42ab6977ccf302be787006
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-09-29 14:12:28 -0600

    MaxOfTwo

---
+++ week1/day1/maxoftwo (solved).js
@@ -0,0 +1,12 @@
+let x = 2
+let y = 2
+
+// Write your code below this line.
+
+if (x > y) {
+    console.log (x)
+} else if (x == y) {
+    console.log ("Both Integers are" , x)
+} else {
+    console.log (y)
+}

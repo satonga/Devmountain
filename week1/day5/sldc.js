57a4adeaa65ff1645351bf429fa8710cf57f3dbe
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-10-01 07:16:25 -0600

    SLDC Assessment

---
+++ week1/day5/sldc.js
@@ -0,0 +1,27 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+step1 = `Planning`
+step2 = `Analysis`
+step3 = `Design`
+step4 = `Implementation`
+step5 = `Testing & Intergration`
+step6 = `Maintain`
+
+console.log(step1, step2, step3, step4, step5, step6)

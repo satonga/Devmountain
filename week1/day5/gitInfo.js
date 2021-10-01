3ba6544c94b1f8bb944c9fe4686d88b1fec8aee9
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-10-01 12:14:14 -0600

    GitInfo assessments

--- week1/day5/gitInfo.js
+++ week1/day5/gitInfo.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = `Git is a command-line interface that allows reallocation, or status checks, of directories or repositories.`
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = `GitHub is the remote repository location where work is stored from local repostories.`
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +34,7 @@
 */
 
 //CODE HERE
-
+let init = `Git init is the intitalized link between the working directory and the remote repo; git init`
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,7 +42,7 @@
 */
 
 //CODE HERE
-
+let clone = `Git clone is a tool used to clone work from github and create a working directory of it on your computer; git clone`
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,7 +50,7 @@
 */
 
 //CODE HERE
-
+let gitStatus = `Git status displays the state of the working directory and staging area; git status`
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +58,7 @@
 */
 
 //CODE HERE
-
+let add = `Git add includes a change in the working directory to the staging area; git add .`
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +66,7 @@
 */
 
 //CODE HERE
-
+let commit = `Git commit captures a snaphsot of the projects currently staged changes; git commit -m "title"`
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -75,3 +75,4 @@
 */
 
 //CODE HERE
+let push = (`Git push is used to upload content from the local repo to the remote repo; git push`)

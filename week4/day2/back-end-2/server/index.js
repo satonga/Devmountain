904b7c30aeed5423a9522de5dbdfe12750761a00
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-10-19 14:27:39 -0600

    server

---
+++ week4/day2/back-end-2/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const ctrl = require('./controller')
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+app.get('/api/houses', ctrl.getHouses)
+app.post('/api/houses', ctrl.createHouse)
+app.put('/api/houses/:id', ctrl.updateHouse)
+app.delete('/api/houses/:id', ctrl.deleteHouse)
+
+
+app.listen(4004, () => console.log('Server listening on port 4004.'))

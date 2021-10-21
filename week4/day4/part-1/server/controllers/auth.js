65b942fb240caf6935426eae01f1df0d3a901f37
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-10-21 14:47:14 -0600

    lab part-1

---
+++ week4/day4/part-1/server/controllers/auth.js
@@ -0,0 +1,35 @@
+const users = []
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+    login: (req, res) => {
+      const { username, password } = req.body
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].username === username) {
+          const authenticated = bcrypt.compareSync(password, users[i].passwordHash)
+          if(authenticated) {
+            let userToReturn = {...users[i]}
+            delete userToReturn.passwordHash
+            res.status(200).send(users[i])
+          }
+        }
+      }
+      res.status(400).send("User not found.")
+    },
+    register: (req, res) => {
+        const {email, username, firstName, lastName, password} = req.body
+        const salt = bcrypt.genSaltSync(5)
+        const passwordHash = bcrypt.hashSync(password, salt)
+        let user = {
+          email,
+          username,
+          firstName,
+          lastName,
+          passwordHash
+        }
+        users.push(user)
+        let userToReturn = {...user}
+        delete userToReturn.passwordHash
+        res.status(200).send(userToReturn)
+    }
+}

933c50c20538782a6f376ef4e577a75f096856e4
satonga <spencer.tonga18@gmail.comgit config --global user.name satonga>
2021-10-28 07:26:49 -0600

    subqueries

---
+++ week5/day3/subqueries.sql
@@ -0,0 +1,17 @@
+SELECT * FROM invoice_line
+    WHERE unit_price > 0.99;
+
+SELECT * FROM playlist
+    WHERE name = 'Music';
+
+SELECT * FROM playlist
+    WHERE playlist_id = 5;
+
+SELECT * FROM genre
+    WHERE name = 'Comedy';
+
+SELECT * FROM album
+WHERE title = 'Fireball';
+
+SELECT * FROM track
+WHERE composer = 'Queen';
